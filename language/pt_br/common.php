18c3a67f5f9ecf6464b14aacd74725ab59dac18b
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-04-12 11:33:30 -0300

    UPDATE

--- language/pt_br/common.php
+++ language/pt_br/common.php
@@ -2,10 +2,10 @@
 /**
  *
  * Best Answer extension for the phpBB Forum Software package.
- *
+ * Brazilian Portuguese translation by eunaumtenhoid [2017][ver 1.0.0] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2018, kinerity, https://www.layer-3.org/
  * @license GNU General Public License, version 2 (GPL-2.0)
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.0] (https://github.com/phpBBTraducoes)
+ * 
  */
 
 /**
